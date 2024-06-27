31cc935909f378154066aec0f8ab59a95e92094e
alan-soka <jevallejo13@gmail.com>
2024-06-27 13:26:03 +0200

    agregamos informacion en html y css de article

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -9,18 +9,6 @@ class BandaCard extends HTMLElement {
     set banda(banda) {
         this.shadowRoot.innerHTML = `
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-
         `;
     }
 }
@@ -36,7 +24,7 @@ fetch('../JAVASCRIPT/articles.json')
         data.bandas.forEach(banda => {
             const bandaCard = document.createElement('banda-card');
             bandaCard.banda = banda;
-            container.appendChild(bandaCard);
+            container.appendChild(bandaCard); 
         });
     })
     .catch(error => console.error('Error al cargar los datos:', error));
